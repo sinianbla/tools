eb72a0eb0870dcd58b50b68101f689eac3921706
mawei <mawei@thinkinfo.tech>
2018-08-01 16:14:15 +0800

    add service

--- src/main/java/com/sinianbla/controller/UserController.java
+++ src/main/java/com/sinianbla/controller/UserController.java
@@ -3,6 +3,8 @@ package com.sinianbla.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.sinianbla.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,11 +12,14 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class UserController {
 	
-	
+	@Autowired
+	private UserService userService;
+
 	@RequestMapping("/user/login")
 	public ModelAndView login(HttpServletRequest req, HttpServletResponse res){
 		
 		ModelAndView mv = new ModelAndView("view/login");
+		userService.add();
 //		mv.addObject("name", req.getCookies()[0].getValue());
 		return mv;
 	}
---
+++ src/main/java/com/sinianbla/service/UserService.java
@@ -0,0 +1,10 @@
+package com.sinianbla.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public void add(){
+        System.out.print("service................");
+    }
+}
